4f99fc7894607921346f120dd29a5229c3690ea8
giorgio <i@gvdev.com>
2019-10-28 20:02:11 +0100

    Construction speed improved significantly. Fixed #3

--- include/piecewise_linear_model.hpp
+++ include/piecewise_linear_model.hpp
@@ -16,7 +16,6 @@
 
 #pragma once
 
-#include <list>
 #include <cmath>
 #include <limits>
 #include <vector>
@@ -57,13 +56,19 @@ private:
         bool operator>(Point p) const {
             return y * p.x > x * p.y;
         }
+
+        bool operator==(Point p) const {
+            return y * p.x == x * p.y;
+        }
     };
 
     const SY error_fwd;
     const SY error_bwd;
-    std::list<Point> lower;
-    std::list<Point> upper;
-    size_t points_in_hull{};
+    std::vector<Point> lower;
+    std::vector<Point> upper;
+    size_t lower_start = 0;
+    size_t upper_start = 0;
+    size_t points_in_hull = 0;
     Point rectangle[4];
 
     template<typename P>
@@ -71,15 +76,12 @@ private:
         return (A.x - O.x) * (B.y - O.y) - (A.y - O.y) * (B.x - O.x);
     }
 
-    auto minmax(std::list<Point> &hull, Point &s) {
-        return std::minmax_element(hull.cbegin(), hull.cend(), [&s](auto &u, auto &v) {
-            return (u - s) < (v - s);
-        });
-    }
-
 public:
     explicit OptimalPiecewiseLinearModel(size_t error_fwd, size_t error_bwd)
-        : error_fwd(error_fwd), error_bwd(error_bwd) {}
+        : error_fwd(error_fwd), error_bwd(error_bwd) {
+        upper.reserve(1u << 16);
+        lower.reserve(1u << 16);
+    }
 
     bool add_point(X x, Y y) {
         if (x < rectangle[2].x || x < rectangle[3].x)
@@ -87,6 +89,7 @@ public:
 
         SX xx = x;
         SY yy = y;
+        
         if (points_in_hull == 0) {
             rectangle[0] = {xx, yy + error_fwd};
             rectangle[1] = {xx, yy - error_bwd};
@@ -97,17 +100,21 @@ public:
         if (points_in_hull == 1) {
             rectangle[2] = {xx, yy - error_bwd};
             rectangle[3] = {xx, yy + error_fwd};
-            upper = {rectangle[0], rectangle[3]};
-            lower = {rectangle[1], rectangle[2]};
+            upper.clear();
+            upper.push_back(rectangle[0]);
+            upper.push_back(rectangle[3]);
+            lower.clear();
+            lower.push_back(rectangle[1]);
+            lower.push_back(rectangle[2]);
+            upper_start = lower_start = 0;
             ++points_in_hull;
             return true;
         }
 
-        auto slope1 = rectangle[2] - rectangle[0];
-        auto slope2 = rectangle[3] - rectangle[1];
-
         Point p1(xx, yy + error_fwd);
         Point p2(xx, yy - error_bwd);
+        auto slope1 = rectangle[2] - rectangle[0];
+        auto slope2 = rectangle[3] - rectangle[1];
         bool outside_line1 = p1 - rectangle[2] < slope1;
         bool outside_line2 = p2 - rectangle[3] > slope2;
 
@@ -117,32 +124,52 @@ public:
         }
 
         if (p1 - rectangle[1] < slope2) {
-            auto m = minmax(lower, p1).first;
-            auto new_slope = p1 - *m;
-            if (new_slope < slope2) {
-                rectangle[1] = *m;
-                rectangle[3] = p1;
-                lower.erase(lower.begin(), m);
+            // Find extreme slope
+            Point min = lower[lower_start] - p1;
+            size_t min_i = lower_start;
+            for (size_t i = lower_start + 1; i < lower.size(); i++) {
+                auto val = (lower[i] - p1);
+                if (val > min)
+                    break;
+                else {
+                    min = val;
+                    min_i = i;
+                }
             }
 
+            rectangle[1] = lower[min_i];
+            rectangle[3] = p1;
+            lower_start = min_i;
+
             // Hull update
-            while (upper.size() >= 2 && cross(*std::prev(upper.end(), 2), *std::prev(upper.end()), p1) <= 0)
-                upper.pop_back();
+            size_t end = upper.size();
+            for (; end >= upper_start + 2 && cross(upper[end - 2], upper[end - 1], p1) <= 0; --end);
+            upper.resize(end);
             upper.push_back(p1);
         }
 
         if (p2 - rectangle[0] > slope1) {
-            auto m = minmax(upper, p2).second;
-            auto new_slope = p2 - *m;
-            if (slope1 < new_slope) {
-                rectangle[0] = *m;
-                rectangle[2] = p2;
-                upper.erase(upper.begin(), m);
+            // Find extreme slope
+            Point max = upper[upper_start] - p2;
+            size_t max_i = upper_start;
+            for (size_t i = upper_start + 1; i < upper.size(); i++) {
+                auto val = (upper[i] - p2);
+                if (val < max)
+                    break;
+                else {
+                    max = val;
+                    max_i = i;
+                }
             }
 
+            rectangle[0] = upper[max_i];
+            rectangle[2] = p2;
+            upper_start = max_i;
+
             // Hull update
-            while (lower.size() >= 2 && cross(*std::prev(lower.end(), 2), *std::prev(lower.end()), p2) >= 0)
-                lower.pop_back();
+            size_t end = lower.size();
+            for (; end >= lower_start + 2 && cross(lower[end - 2], lower[end - 1], p2) >= 0; --end);
+            lower.resize(end);
             lower.push_back(p2);
         }
 
@@ -158,7 +185,7 @@ public:
         auto slope1 = p2 - p0;
         auto slope2 = p3 - p1;
 
-        if (points_in_hull == 1 || slope1.x * slope2.y == slope1.y * slope2.x)
+        if (points_in_hull == 1 || slope1 == slope2)
             return std::make_pair(double(p0.x), double(p0.y));
 
         double a = slope1.x * slope2.y - slope1.y * slope2.x;
