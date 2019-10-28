5f2445abf2f04897470d996f907fc49404c1370d
giorgio <i@gvdev.com>
2019-10-28 20:03:29 +0100

    More test cases

--- test/main.cpp
+++ test/main.cpp
@@ -21,18 +21,27 @@
 #include "pgm_index_compressed.hpp"
 #include <type_traits>
 
-TEMPLATE_TEST_CASE("Segmentation algorithm", "", float, uint32_t) {
-    const auto error = 64;
+TEMPLATE_TEST_CASE("Segmentation algorithm", "", float, double, uint32_t, uint64_t) {
+    const auto error = GENERATE(32, 64, 128);
     std::vector<TestType> data(1000000);
     std::random_device rnd_device;
     std::mt19937 engine{rnd_device()};
+    using RandomFunction = std::function<TestType()>;
 
     if constexpr (std::is_floating_point<TestType>()) {
-        std::lognormal_distribution<TestType> dist(0, 0.5);
-        std::generate(data.begin(), data.end(), [&dist, &engine]() { return dist(engine); });
+        RandomFunction lognormal = std::bind(std::lognormal_distribution<TestType>(0, 0.5), engine);
+        RandomFunction exponential = std::bind(std::exponential_distribution<TestType>(1.2), engine);
+        auto rand = GENERATE_COPY(as<RandomFunction>{}, lognormal, exponential);
+        std::generate(data.begin(), data.end(), rand);
     }
-    else
-        std::generate(data.begin(), data.end(), std::rand);
+    else {
+        RandomFunction uniform_dense = std::bind(std::uniform_int_distribution<TestType>(0, 10000), engine);
+        RandomFunction uniform_sparse = std::bind(std::uniform_int_distribution<TestType>(0, 10000000), engine);
+        RandomFunction binomial = std::bind(std::binomial_distribution<TestType>(50000), engine);
+        RandomFunction geometric = std::bind(std::geometric_distribution<TestType>(0.8), engine);
+        auto rand = GENERATE_COPY(as<RandomFunction>{}, uniform_dense, uniform_sparse, binomial, geometric);
+        std::generate(data.begin(), data.end(), rand);
+    }
 
     std::sort(data.begin(), data.end());
     auto segments = Segmentation<TestType, 0>::build_segments(data, error);
@@ -43,7 +52,7 @@ TEMPLATE_TEST_CASE("Segmentation algorithm", "", float, uint32_t) {
             auto pos = std::lower_bound(segments.cbegin(), segments.cend(), key);
             auto segment = pos == segments.cbegin() || key == pos->key ? pos : pos - 1;
             double e = std::fabs(double(i) - std::min(data.size() - 1, segment->operator()(key)));
-            REQUIRE(e <= error);
+            REQUIRE(e <= error + 1);
         }
     }
 }
