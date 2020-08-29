feb67e45a51eb34ce98424702935b0fe4ca1e57f
giorgio <i@gvdev.com>
2020-08-29 12:15:36 +0200

    Faster incremental compilation of tests

---
+++ test/main.cpp
@@ -0,0 +1,3 @@
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_FAST_COMPILE
+#include "catch.hpp"
--- test/tests.cpp
+++ test/tests.cpp
@@ -13,9 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#define CATCH_CONFIG_MAIN
-#define CATCH_CONFIG_FAST_COMPILE
-
 #include <map>
 #include <random>
 #include <functional>
