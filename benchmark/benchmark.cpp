53323a4dac7763662ae071b92a4fcf7537849318
giorgio <i@gvdev.com>
2021-03-22 09:22:08 +0100

    Added benchmark

---
+++ benchmark/benchmark.cpp
@@ -0,0 +1,122 @@
+// This file is part of PGM-index <https://github.com/gvinciguerra/PGM-index>.
+// Copyright (c) 2021 Giorgio Vinciguerra.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "args.hxx"
+#include "benchmark.hpp"
+#include "pgm/pgm_index.hpp"
+#include "pgm/pgm_index_variants.hpp"
+
+#define FOR_EACH_EPSILON(C, K) C<K, 8>, C<K, 16>, C<K, 32>, C<K, 64>, C<K, 128>, C<K, 256>, \
+                               C<K, 512>, C<K, 1024>, C<K, 2048>, C<K, 4096>
+
+#define PGM_CLASSES(K) FOR_EACH_EPSILON(pgm::PGMIndex, K)
+#define BPGM_CLASSES(K) FOR_EACH_EPSILON(pgm::BucketingPGMIndex, K)
+#define EFPGM_CLASSES(K) FOR_EACH_EPSILON(pgm::EliasFanoPGMIndex, K)
+#define CPGM_CLASSES(K) FOR_EACH_EPSILON(pgm::CompressedPGMIndex, K)
+
+#define ALL_CLASSES(K) PGM_CLASSES(K), BPGM_CLASSES(K), EFPGM_CLASSES(K), CPGM_CLASSES(K)
+
+template<typename K>
+void read_ints_helper(args::PositionalList<std::string> &files,
+                      size_t record_size,
+                      double lookup_ratio,
+                      const std::string &workload) {
+    OUT_VERBOSE("Running with " << sizeof(K) << "-byte keys + " << record_size - sizeof(K) << "-byte values")
+    for (const auto &file : files.Get()) {
+        auto data = to_records(read_data_binary<K>(file, true), record_size);
+        auto filename = file.substr(file.find_last_of("/\\") + 1);
+        benchmark_all<K, ALL_CLASSES(K)>(filename, data, record_size, lookup_ratio, workload);
+    }
+}
+
+int main(int argc, char **argv) {
+    using namespace args;
+    ArgumentParser p("Benchmark for the PGM-index library.");
+    p.helpParams.helpindent = 25;
+    CompletionFlag completion(p, {"complete"});
+    HelpFlag help(p, "help", "Display this help menu", {'h', "help"});
+    Flag verbose(p, "", "Verbose output", {'v', "verbose"});
+    ValueFlag<size_t> value_size(p, "bytes", "Size of the values associated to keys", {'V', "values"}, 0);
+
+    Group g1(p, "QUERY WORKLOAD OPTIONS (mutually exclusive):", Group::Validators::AtMostOne);
+    ValueFlag<double> ratio(g1, "ratio", "Random workload with the given lookup ratio", {'r', "ratio"}, 0.333);
+    ValueFlag<std::string> workload(g1, "file", "Custom workload file. Obeys the format of input files", {'w', "workload"});
+
+    Group g2(p, "INPUT DATA OPTIONS (mutually exclusive):", Group::Validators::Xor, Options::Required);
+    ValueFlag<size_t> synthetic(g2, "size", "Generate synthetic data of the given size", {'s', "synthetic"}, 100000000);
+    Flag u64(g2, "", "Input files contain unsigned 64-bit ints", {'U', "u64"});
+    Flag i64(g2, "", "Input files contain signed 64-bit ints", {'I', "i64"});
+    PositionalList<std::string> files(p, "file", "The input files");
+
+    try {
+        p.ParseCLI(argc, argv);
+    }
+    catch (args::Completion &e) {
+        std::cout << e.what();
+        return 0;
+    }
+    catch (args::Help &) {
+        std::cout << p;
+        return 0;
+    }
+    catch (args::Error &e) {
+        std::cerr << e.what() << std::endl;
+        std::cerr << p;
+        return 1;
+    }
+
+    if (ratio.Get() < 0 || ratio.Get() > 1) {
+        std::cerr << "Argument to --" << ratio.GetMatcher().GetLongOrAny().str() << " must be between 0.0 and 1.0.";
+        return 1;
+    }
+
+    if (synthetic && synthetic.Get() < 1000) {
+        std::cerr << "Argument to --" << synthetic.GetMatcher().GetLongOrAny().str() << " must be greater than 1000.";
+        return 1;
+    }
+
+    global_verbose = verbose.Get();
+    std::cout << "dataset,class_name,build_ms,bytes,query_ns" << std::endl;
+
+    if (synthetic) {
+        auto record_size = value_size.Get() + sizeof(uint64_t);
+        auto n = synthetic.Get();
+        std::mt19937 generator(std::random_device{}());
+        auto gen = [&](auto distribution) {
+            std::vector<uint64_t> out(n);
+            std::generate(out.begin(), out.end(), [&] { return distribution(generator); });
+            std::sort(out.begin(), out.end());
+            return to_records(out, record_size);
+        };
+        std::vector<std::pair<std::string, std::function<std::vector<char>()>>> distributions = {
+            {"uniform_dense", std::bind(gen, std::uniform_int_distribution<uint64_t>(0, n * 1000))},
+            {"uniform_sparse", std::bind(gen, std::uniform_int_distribution<uint64_t>(0, n * n))},
+            {"binomial", std::bind(gen, std::binomial_distribution<uint64_t>(1ull << 50))},
+            {"negative_binomial", std::bind(gen, std::negative_binomial_distribution<uint64_t>(1ull << 50, 0.3))},
+            {"geometric", std::bind(gen, std::geometric_distribution<uint64_t>(1e-10))},
+        };
+        OUT_VERBOSE("Generating " << to_metric(n) << " elements (8-byte keys + " << value_size.Get() << "-byte values)")
+        OUT_VERBOSE("Total memory for data is " << to_metric(n * record_size, 2, true) << "B")
+        for (auto&[name, gen_data] : distributions)
+            benchmark_all<uint64_t, ALL_CLASSES(uint64_t)>(name, gen_data(), record_size, ratio.Get(), workload.Get());
+    }
+
+    if (i64.Get())
+        read_ints_helper<int64_t>(files, value_size.Get() + sizeof(int64_t), ratio.Get(), workload.Get());
+    if (u64.Get())
+        read_ints_helper<uint64_t>(files, value_size.Get() + sizeof(uint64_t), ratio.Get(), workload.Get());
+
+    return 0;
+}
---
+++ benchmark/benchmark.hpp
@@ -0,0 +1,277 @@
+// This file is part of PGM-index <https://github.com/gvinciguerra/PGM-index>.
+// Copyright (c) 2021 Giorgio Vinciguerra.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <cstdio>
+#include <cassert>
+#include <cstdint>
+#include <vector>
+#include <chrono>
+#include <random>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <filesystem>
+#include <type_traits>
+
+bool global_verbose = false;
+
+#define IF_VERBOSE(X) if (global_verbose) { X; }
+#define OUT_VERBOSE(X) if (global_verbose) { std::cout << "# " << X << std::endl; }
+
+using timer = std::chrono::high_resolution_clock;
+
+template<typename T>
+std::string to_metric(const T &x, int digits = 2, bool space = false) {
+    static const char *prefix[] = {"y", "z", "a", "f", "p", "n", "µ", "m", "", "k", "M", "G", "T", "P", "E", "Z", "Y"};
+    double value = x;
+    if (value < 0.)
+        value = -value;
+
+    auto log_x = (int) std::log10(value);
+    if (log_x > 0)
+        log_x = (log_x / 3) * 3;
+    else
+        log_x = (-log_x + 3) / 3 * (-3);
+
+    value *= pow(10, -log_x);
+    if (value >= 1000.)
+        value /= 1000.0, log_x += 3;
+
+    if (std::fmod(value, 1.0) == 0.0)
+        digits = 0;
+
+    constexpr auto prefix_start = -24;
+    const char *fmt = space ? (x < 0. ? "-%.*f %s" : "%.*f %s") : (x < 0. ? "-%.*f%s" : "%.*f%s");
+    const char *chosen_prefix = prefix[(log_x - prefix_start) / 3];
+    std::vector<char> buffer(100);
+    int size = std::snprintf(&buffer[0], buffer.size(), fmt, digits, value, chosen_prefix);
+    return std::string(&buffer[0], size);
+}
+
+template<typename T>
+std::vector<T> read_data_binary(const std::string &filename, bool check_sorted) {
+    OUT_VERBOSE("Reading " << filename)
+    std::vector<T> data;
+    try {
+        std::fstream in(filename, std::ios::in | std::ios::binary);
+        in.exceptions(std::ios::failbit | std::ios::badbit);
+        size_t file_size = std::filesystem::file_size(filename);
+        data.resize(file_size / sizeof(T));
+        in.read((char *) data.data(), file_size);
+    } catch (std::ios_base::failure &e) {
+        std::cerr << "Could not read the file. " << e.what() << std::endl;
+        exit(1);
+    }
+
+    if (size_t(data[0]) == data.size() - 1)
+        data.erase(data.begin()); // in some formats, the first element is the size of the dataset, ignore it
+
+    if (check_sorted && !std::is_sorted(data.begin(), data.end())) {
+        std::cerr << "Input data must be sorted." << std::endl;
+        std::cerr << "Read: [";
+        std::copy(data.begin(), std::min(data.end(), data.begin() + 10), std::ostream_iterator<T>(std::cerr, ", "));
+        std::cout << "...]." << std::endl;
+        exit(1);
+    }
+
+    IF_VERBOSE(std::cout << "# Read " << to_metric(data.size()) << " elements: [")
+    IF_VERBOSE(std::copy(data.begin(),
+                         std::min(data.end() - 1, data.begin() + 5),
+                         std::ostream_iterator<T>(std::cout, ", ")))
+    IF_VERBOSE(std::cout << "..., " << *(data.end() - 1) << "]" << std::endl)
+
+    return data;
+}
+
+template<typename K>
+std::vector<char> to_records(const std::vector<K> &keys, size_t record_size) {
+    if (record_size < sizeof(K))
+        throw std::runtime_error("record_size < key size");
+    std::vector<char> out(keys.size() * record_size);
+    auto *ptr = out.data();
+    for (auto k : keys) {
+        *reinterpret_cast<K *>(ptr) = k;
+        ptr += record_size;
+    }
+    return out;
+}
+
+#ifdef __GNUG__
+#include <memory>
+#include <cstdlib>
+#include <cxxabi.h>
+
+std::string demangle(const char *name) {
+    int status = 0;
+    std::unique_ptr<char, void (*)(void *)> res{abi::__cxa_demangle(name, nullptr, nullptr, &status), std::free};
+    return status ? name : res.get();
+}
+
+#else
+std::string demangle(const char* name) { return name; }
+#endif
+
+template<typename T>
+class RecordIterator {
+    const char *ptr;
+    size_t step;
+
+public:
+    using value_type = T;
+    using difference_type = std::make_signed_t<size_t>;
+    using reference = const T &;
+    using pointer = const T *;
+    using iterator_category = std::random_access_iterator_tag;
+
+    RecordIterator(const char *ptr, size_t step) : ptr(ptr), step(step) {}
+    RecordIterator(const RecordIterator &other) : ptr(other.ptr), step(other.step) {}
+
+    RecordIterator &operator=(const RecordIterator &other) {
+        ptr = other.ptr;
+        step = other.step;
+        return *this;
+    }
+
+    reference operator*() { return *reinterpret_cast<const T *>(ptr); }
+    reference operator[](difference_type i) { return *reinterpret_cast<const T *>(ptr + i * step); }
+
+    RecordIterator &operator++() {
+        ptr += step;
+        return *this;
+    }
+
+    RecordIterator &operator--() {
+        ptr -= step;
+        return *this;
+
+    }
+
+    RecordIterator operator++(int) {
+        RecordIterator it(*this);
+        ++(*this);
+        return it;
+    }
+
+    RecordIterator operator--(int) {
+        RecordIterator it(*this);
+        --(*this);
+        return it;
+    }
+
+    RecordIterator operator+(difference_type n) const { return {ptr + n * step, step}; }
+    RecordIterator operator-(difference_type n) const { return {ptr - n * step, step}; }
+
+    RecordIterator &operator+=(difference_type n) {
+        ptr += n * step;
+        return *this;
+    }
+
+    RecordIterator &operator-=(difference_type n) {
+        ptr -= n * step;
+        return *this;
+    }
+
+    difference_type operator-(const RecordIterator &it) const {
+        assert(step == it.step);
+        return (ptr - it.ptr) / it.step;
+    }
+
+    bool operator<(const RecordIterator &it) const { return *this - it < 0; }
+    bool operator>(const RecordIterator &it) const { return it < *this; }
+    bool operator<=(const RecordIterator &it) const { return !(*this > it); }
+    bool operator>=(const RecordIterator &it) const { return !(*this < it); }
+    bool operator!=(const RecordIterator &it) const { return !(*this == it); }
+    bool operator==(const RecordIterator &it) const { return *this - it == 0; }
+};
+
+template<typename Class, typename RandomIt>
+std::tuple<uint64_t, uint64_t, size_t>
+benchmark(RandomIt begin, RandomIt end, const std::vector<typename RandomIt::value_type> &queries) {
+    auto t0 = timer::now();
+    Class index(begin, end);
+    auto t1 = timer::now();
+    auto build_ms = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count();
+
+    auto t2 = timer::now();
+    uint64_t cnt = 0;
+    for (auto &q : queries) {
+        auto range = index.search(q);
+        auto lo = begin + range.lo;
+        auto hi = begin + range.hi;
+        cnt += std::distance(begin, std::lower_bound(lo, hi, q));
+    }
+    [[maybe_unused]] volatile auto tmp = cnt;
+    auto t3 = timer::now();
+    auto query_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(t3 - t2).count() / queries.size();
+
+    return {build_ms, query_ns, index.size_in_bytes()};
+}
+
+template<typename RandomIt>
+std::vector<typename RandomIt::value_type>
+generate_queries(RandomIt first, RandomIt last, double lookup_ratio, size_t max_queries = 10000000) {
+    using value_type = typename RandomIt::value_type;
+    auto n = std::distance(first, last);
+    auto num_queries = std::min<size_t>(n / 10, max_queries);
+    auto num_lookups = size_t(num_queries * lookup_ratio);
+    std::uniform_int_distribution<value_type> key_distribution(*first, *std::prev(last));
+    std::uniform_int_distribution<value_type> pos_distribution(0, n - 1);
+    std::mt19937 generator(std::random_device{}());
+    std::vector<value_type> queries;
+    queries.reserve(num_queries);
+
+    for (size_t i = 0; i < num_lookups; ++i)
+        queries.push_back(first[pos_distribution(generator)]);
+    for (size_t i = 0; i < num_queries - num_lookups; ++i)
+        queries.push_back(key_distribution(generator));
+
+    std::shuffle(queries.begin(), queries.end(), generator);
+    return queries;
+}
+
+template<typename T>
+struct type_wrapper { using type = T; };
+
+template<typename... Ts, typename TF>
+void for_types(TF &&f) { (f(type_wrapper<Ts>{}), ...); }
+
+template<typename K, typename... Args>
+void benchmark_all(const std::string &filename,
+                   const std::vector<char> &data,
+                   size_t record_size,
+                   double lookup_ratio,
+                   const std::string &workload) {
+    auto begin = RecordIterator<K>(data.data(), record_size);
+    auto end = RecordIterator<K>(data.data() + data.size(), record_size);
+
+    std::vector<K> queries;
+    if (!workload.empty())
+        queries = read_data_binary<K>(workload, false);
+    else {
+        queries = generate_queries(begin, end, lookup_ratio);
+        auto m = queries.size();
+        OUT_VERBOSE("Generated " << to_metric(m) << " queries, " << to_metric(m * lookup_ratio) << " are lookups")
+    }
+
+    for_types<Args...>([&](auto t) {
+        using class_type = typename decltype(t)::type;
+        auto name = demangle(typeid(class_type).name());
+        auto[build_ms, query_ns, bytes] = benchmark<class_type>(begin, end, queries);
+        std::cout << filename << ",\"" << name << "\"," << build_ms << "," << bytes << "," << query_ns << std::endl;
+    });
+}
