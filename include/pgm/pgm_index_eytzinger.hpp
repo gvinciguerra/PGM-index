ead2dcd399ddb24d769c9cce780ae7721cc25ff5
Роман Агеев <ageevroma2000@gmail.com>
2021-04-12 22:35:24 +0300

    added inheritance

--- include/pgm/pgm_index_eytzinger.hpp
+++ include/pgm/pgm_index_eytzinger.hpp
@@ -54,73 +54,14 @@ namespace pgm {
  * @tparam Floating the floating-point type to use for slopes
  */
 template<typename K, size_t Epsilon = 64, typename Floating = float>
-class PGMIndexEytzinger {
+class PGMIndexEytzinger : public PGMIndex<K, Epsilon, 0, Floating> {
 protected:
-    template<typename, size_t, uint8_t, typename>
-    friend class BucketingPGMIndex;
-
-    template<typename, size_t, typename>
-    friend class EliasFanoPGMIndex;
-
-    static_assert(Epsilon > 0);
-    struct Segment;
     struct IndexIterator;
     struct IdxHolder;
 
-    size_t n;                           ///< The number of elements this index was built on.
-    K first_key;                        ///< The smallest element.
-    std::vector<Segment> segments;      ///< The segments composing the index.
-    std::vector<size_t> levels_offsets; ///< The starting position of each level in segments[], in reverse order.
+    using Segment = typename PGMIndex<K, Epsilon, 0,Floating>::Segment;
+
     EytzingerArray<IdxHolder> eytzinger_first_layer;
-
-    template<typename RandomIt>
-    static void build(RandomIt first, RandomIt last,
-                      size_t epsilon, size_t epsilon_recursive,
-                      std::vector<Segment> &segments,
-                      std::vector<size_t> &levels_offsets) {
-        auto n = (size_t) std::distance(first, last);
-        if (n == 0)
-            return;
-
-        levels_offsets.push_back(0);
-        segments.reserve(n / (epsilon * epsilon));
-
-        auto ignore_last = *std::prev(last) == std::numeric_limits<K>::max(); // max() is the sentinel value
-        auto last_n = n - ignore_last;
-        last -= ignore_last;
-
-        auto build_level = [&](auto epsilon, auto in_fun, auto out_fun) {
-            auto n_segments = internal::make_segmentation_par(last_n, epsilon, in_fun, out_fun);
-            if (segments.back().slope == 0) {
-                // Here we need to ensure that keys > *(last-1) are approximated to a position == prev_level_size
-                segments.emplace_back(*std::prev(last) + 1, 0, last_n);
-                ++n_segments;
-            }
-            segments.emplace_back(last_n); // Add the sentinel segment
-            return n_segments;
-        };
-
-        // Build first level
-        auto in_fun = [&](auto i) {
-            auto x = first[i];
-            // Here there is an adjustment for inputs with duplicate keys: at the end of a run of duplicate keys equal
-            // to x=first[i] such that x+1!=first[i+1], we map the values x+1,...,first[i+1]-1 to their correct rank i
-            auto flag = i > 0 && i + 1u < n && x == first[i - 1] && x != first[i + 1] && x + 1 != first[i + 1];
-            return std::pair<K, size_t>(x + flag, i);
-        };
-        auto out_fun = [&](auto cs) { segments.emplace_back(cs); };
-        last_n = build_level(epsilon, in_fun, out_fun);
-        levels_offsets.push_back(levels_offsets.back() + last_n + 1);
-
-        // Build upper levels
-        while (epsilon_recursive && last_n > 1) {
-            auto offset = levels_offsets[levels_offsets.size() - 2];
-            auto in_fun_rec = [&](auto i) { return std::pair<K, size_t>(segments[offset + i].key, i); };
-            last_n = build_level(epsilon_recursive, in_fun_rec, out_fun);
-            levels_offsets.push_back(levels_offsets.back() + last_n + 1);
-        }
-    }
-
     /**
      * Returns the segment responsible for a given key, that is, the rightmost segment having key <= the sought key.
      * @param key the value of the element to search for
@@ -128,14 +69,11 @@ protected:
      */
     auto segment_for_key(const K &key) const {
       auto idx = eytzinger_first_layer.search(IdxHolder(key, 0));
-      auto it = segments.begin() + ((idx == eytzinger_first_layer.size()) ? segments_count() : eytzinger_first_layer[idx].idx);
-      return it == segments.begin() ? it : std::prev(it);
+      auto it = this->segments.begin() + ((idx == eytzinger_first_layer.size()) ? this->segments_count() : eytzinger_first_layer[idx].idx);
+      return it == this->segments.begin() ? it : std::prev(it);
     }
 
 public:
-
-    static constexpr size_t epsilon_value = Epsilon;
-
     /**
      * Constructs an empty index.
      */
@@ -145,95 +83,27 @@ public:
      * Constructs the index on the given sorted vector.
      * @param data the vector of keys to be indexed, must be sorted
      */
-    explicit PGMIndexEytzinger(const std::vector<K> &data) : PGMIndexEytzinger(data.begin(), data.end()) {}
+    explicit PGMIndexEytzinger(const std::vector<K> &data) : PGMIndex<K, Epsilon, 0, Floating>(data.begin(), data.end()) {}
 
     /**
      * Constructs the index on the sorted keys in the range [first, last).
      * @param first, last the range containing the sorted keys to be indexed
      */
     template<typename RandomIt>
-    PGMIndexEytzinger(RandomIt first, RandomIt last)
-        : n(std::distance(first, last)), eytzinger_first_layer(),
-          first_key(n ? *first : 0),
-          segments(),
-          levels_offsets() {
-        build(first, last, Epsilon, 0, segments, levels_offsets);
-        auto iter = IndexIterator(segments.begin());
-        eytzinger_first_layer = EytzingerArray<IdxHolder>(iter, segments_count());
+    PGMIndexEytzinger(RandomIt first, RandomIt last) :
+        PGMIndex<K, Epsilon, 0, Floating>(first, last), eytzinger_first_layer() {
+        auto iter = IndexIterator(this->segments.begin());
+        eytzinger_first_layer = EytzingerArray<IdxHolder>(iter, this->segments_count());
     }
-
-    /**
-     * Returns the approximate position and the range where @p key can be found.
-     * @param key the value of the element to search for
-     * @return a struct with the approximate position and bounds of the range
-     */
-    ApproxPos search(const K &key) const {
-        auto k = std::max(first_key, key);
-        auto it = segment_for_key(k);
-        auto pos = std::min<size_t>((*it)(k), std::next(it)->intercept);
-        auto lo = PGM_SUB_EPS(pos, Epsilon);
-        auto hi = PGM_ADD_EPS(pos, Epsilon, n);
-        return {pos, lo, hi};
-    }
-
-    /**
-     * Returns the number of segments in the last level of the index.
-     * @return the number of segments
-     */
-    size_t segments_count() const { return segments.empty() ? 0 : levels_offsets[1] - 1; }
-
-    /**
-     * Returns the number of levels of the index.
-     * @return the number of levels of the index
-     */
-    size_t height() const { return levels_offsets.size() - 1; }
 
     /**
      * Returns the size of the index in bytes.
      * @return the size of the index in bytes
      */
-    size_t size_in_bytes() const { return segments.size() * sizeof(Segment) + levels_offsets.size() * sizeof(size_t); }
+    size_t size_in_bytes() const { return this->segments.size() * sizeof(Segment) + this->levels_offsets.size() * sizeof(size_t) + eytzinger_first_layer.size() * sizeof(IdxHolder); }
 };
 
 #pragma pack(push, 1)
-
-template<typename K, size_t Epsilon, typename Floating>
-struct PGMIndexEytzinger<K, Epsilon, Floating>::Segment {
-    K key;             ///< The first key that the segment indexes.
-    Floating slope;    ///< The slope of the segment.
-    int32_t intercept; ///< The intercept of the segment.
-
-    Segment() = default;
-
-    Segment(K key, Floating slope, int32_t intercept) : key(key), slope(slope), intercept(intercept) {};
-
-    explicit Segment(size_t n) : key(std::numeric_limits<K>::max()), slope(), intercept(n) {};
-
-    explicit Segment(const typename internal::OptimalPiecewiseLinearModel<K, size_t>::CanonicalSegment &cs)
-        : key(cs.get_first_x()) {
-        auto[cs_slope, cs_intercept] = cs.get_floating_point_segment(key);
-        if (cs_intercept > std::numeric_limits<decltype(intercept)>::max())
-            throw std::overflow_error("Change the type of Segment::intercept to int64");
-        slope = cs_slope;
-        intercept = cs_intercept;
-    }
-
-    friend inline bool operator<(const Segment &s, const K &k) { return s.key < k; }
-    friend inline bool operator<(const K &k, const Segment &s) { return k < s.key; }
-    friend inline bool operator<(const Segment &s, const Segment &t) { return s.key < t.key; }
-
-    operator K() { return key; };
-
-    /**
-     * Returns the approximate position of the specified key.
-     * @param k the key whose position must be approximated
-     * @return the approximate position of the specified key
-     */
-    inline size_t operator()(const K &k) const {
-        auto pos = int64_t(slope * (k - key)) + intercept;
-        return pos > 0 ? size_t(pos) : 0ull;
-    }
-};
 
 template<typename K, size_t Epsilon, typename Floating>
 struct PGMIndexEytzinger<K, Epsilon, Floating>::IndexIterator {
